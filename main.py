b78ec403e74a226cb520676576e32bbd4af44730
theGuardianCode <nickrichard878@gmail.com>
2024-01-13 21:38:21 +1000

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode([800, 600])
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((120, 230, 120))
+
+    pygame.display.flip()
